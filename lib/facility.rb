7d7f5bc6d05c523de87203eaa1fafb1c3df76651
Melony Franchini <meltravelz@gmail.com>
2023-09-26 10:46:01 -0600

    feat: #register_vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,30 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services,
+              :registered_vehicles, 
+              :collected_fees
 
   def initialize(facility_info)
     @name = facility_info[:name]
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if vehicle.antique?
+      @collected_fees += 25 
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+    else
+      @collected_fees += 100
+    end
   end
 end
