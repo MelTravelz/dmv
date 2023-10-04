bef0ab6715e351ce017f0a22692341664c2dfa35
Melony Franchini <meltravelz@gmail.com>
2023-10-03 20:09:33 -0600

    fix: add more robust testing

--- spec/dmv_data_service_spec.rb
+++ spec/dmv_data_service_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe DmvDataService do
   before(:each) do
     @dds = DmvDataService.new
   end
+
   describe '#initialize' do
     it 'can initialize' do
       expect(@dds).to be_an_instance_of(DmvDataService)
@@ -22,24 +23,40 @@ RSpec.describe DmvDataService do
   describe '#wa_ev_registrations' do
     it 'can load washington ev registration data' do
       expect(@dds.wa_ev_registrations.size).to be_an(Integer)
+
+      expect(@dds.wa_ev_registrations).to be_an(Array)
+      expect(@dds.wa_ev_registrations[0]).to be_a(Hash)
+      expect(@dds.wa_ev_registrations[0].keys.size).to eq(34)
     end
   end
 
   describe '#co_dmv_office_locations' do
     it 'can load colorado dmv office locations' do
       expect(@dds.co_dmv_office_locations.size).to be_an(Integer)
+
+      expect(@dds.co_dmv_office_locations).to be_an(Array)
+      expect(@dds.co_dmv_office_locations[0]).to be_a(Hash)
+      expect(@dds.co_dmv_office_locations[0].keys.size).to eq(15)
     end
   end
 
   describe '#ny_dmv_office_locations' do
     it 'can load new york dmv office locations' do
       expect(@dds.ny_dmv_office_locations.size).to be_an(Integer)
+
+      expect(@dds.ny_dmv_office_locations).to be_an(Array)
+      expect(@dds.ny_dmv_office_locations[0]).to be_a(Hash)
+      expect(@dds.ny_dmv_office_locations[0].keys.size).to eq(21)
     end
   end
 
   describe '#mo_dmv_office_locations' do
     it 'can load missouri dmv office locations' do
       expect(@dds.mo_dmv_office_locations.size).to be_an(Integer)
+
+      expect(@dds.mo_dmv_office_locations).to be_an(Array)
+      expect(@dds.mo_dmv_office_locations[0]).to be_a(Hash)
+      expect(@dds.mo_dmv_office_locations[0].keys.size).to eq(25)
     end
   end
 end
