89789cc40576c156bbad35fd5a9a358e36999eb4
Melony Franchini <meltravelz@gmail.com>
2023-10-03 20:25:21 -0600

    feat: #create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,4 +3,18 @@ class VehicleFactory
   def initialize
     @dds_service = DmvDataService.new
   end
+
+  def create_vehicles(registrations)
+    registrations.map do |registration|
+      Vehicle.new(
+        vin: registration[:vin_i_10],
+        year: registration[:model_year],
+        make: registration[:make],
+        model: registration[:model],
+        engine: :ev,
+        registration_date: nil,
+        plate_type: nil
+      )
+    end
+  end
 end
