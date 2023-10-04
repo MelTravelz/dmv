31e1ec320b676686f764add0f5c5ac39abf3b13d
Melony Franchini <meltravelz@gmail.com>
2023-10-03 20:24:45 -0600

    test: add #create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe VehicleFactory do
       expect(dds_service).to be_an_instance_of(DmvDataService)
     end
   end
+
+  describe '#create_vehicles' do
+    it 'can create an array of vehicles' do
+      wa_ev_registrations = dds_service.wa_ev_registrations
+      wa_vehicles = factory.create_vehicles(dds_service.wa_ev_registrations)
+
+      expect(wa_vehicles).to be_an(Array)
+      expect(wa_vehicles.size).to eq(1000)
+      expect(wa_vehicles[0]).to be_a(Vehicle)
+    end
+  end
 end
