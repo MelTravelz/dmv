648023aada1e46b0485607a437b77bc9d49a9486
Melony Franchini <meltravelz@gmail.com>
2023-10-03 22:16:00 -0600

    fix: update spaces & comments in #create_facility

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -6,8 +6,8 @@ class FacilityFactory
 
   def create_facility(office_locations)
     office_locations.map do |office|
-
       case office[:state] 
+
       when "CO"
         Facility.new(
           name: office[:dmv_office],
@@ -16,10 +16,11 @@ class FacilityFactory
         )
 
       when "NY"
+        # NOTE: Could refactor into helper methods:
         formatted_phone = ""
         if office[:public_phone_number]
           phone = office[:public_phone_number]
-          formatted_phone = "(" + phone[0, 3] + ")" + phone[3, 3] + "-" + phone[6, 4]
+          formatted_phone = "(" + phone[0, 3] + ") " + phone[3, 3] + "-" + phone[6, 4]
         end
 
         name = "#{office[:office_name]} #{office[:office_type]}"
@@ -27,12 +28,26 @@ class FacilityFactory
 
         address_city = "#{office[:street_address_line_1]} #{office[:city]}"
         capitalized_address_city = address_city.split.map(&:capitalize).join(' ')
-        full_address = capitalized_address_city + "#{office[:state]} #{office[:zip_code]}"
+        full_address = capitalized_address_city + " #{office[:state]} #{office[:zip_code]}"
+        # NOTE: see note above
 
         Facility.new(
           name: capitalized_name,
           address: full_address,
           phone: formatted_phone
+        )
+
+      when "MO"
+        # NOTE: Could refactor into helper methods:
+        address_city = "#{office[:address1]} #{office[:city]}"
+        capitalized_address_city = address_city.split.map(&:capitalize).join(' ')
+        full_address = capitalized_address_city + " #{office[:state]} #{office[:zipcode]}"
+        # NOTE: see note above
+
+        Facility.new(
+          name: "#{office[:name].capitalize} Office",
+          address: full_address,
+          phone: office[:phone]
         )
       end
     end
